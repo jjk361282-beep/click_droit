a95cf7d0a09075c625a15ec71340b97e56d048af
cabrel jordan <ncjjjk1223@gmail.com>
2025-12-18 14:02:06 +0100

    je viens de commencer le projet

---
+++ app.js
@@ -0,0 +1,12 @@
+const lis_action=['orange','red','blue',"#111","#1d7"]
+
+const boitmenue=document.querySelector('.boite_menu')
+
+const btnall=[...document.querySelectorAll('.boite_menu button')]
+
+document.addEventListener("contextmenu",toogmenu)
+
+function toogmenu(event) {
+    console.log(event);
+    
+}
